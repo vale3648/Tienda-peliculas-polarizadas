098607eebc7badf96b7f2cbeab406b61096d48fe
valentina <vaentinacubidesvega@gmail.com>
2025-02-17 13:31:19 -0500

    Arreglo lista de productos

--- src/app/landing/landing/landing.component.ts
+++ src/app/landing/landing/landing.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductoListComponent } from '../../productos/producto-list/producto-list.component';
 
 @Component({
   selector: 'app-landing',
-  imports: [CommonModule],
+  imports: [CommonModule, ProductoListComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
